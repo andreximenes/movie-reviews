625641a7b0481bdb486500db2e16e40f70fdf668
Andre Luiz Ximenes Goncalves da Luz <andre.luz@linx.com.br>
2018-05-30 11:37:09 -0300

    organizada para ficar melhor de Garcia entender

--- src/components/movieInfoComponent.jsx
+++ src/components/movieInfoComponent.jsx
@@ -20,39 +20,34 @@ const HeaderContainer = props => (
 )
 
 // MovieCover usado para listar os filmes, onde o mesmo será um link para acessar os detalhes do filme.
-class MovieCover extends Component {
-  render(){
-    return(
-      <div>
-        <img className="movie-cover" 
-          src={this.props.movie.medium_cover_image} 
-          alt="Carregar Mais..."
-          title={this.props.movie.title} />
-      </div>
-    )
-  }
-}
+const MovieCover = props => (
+  <div>
+    <img className="movie-cover" 
+      src={props.movie.medium_cover_image} 
+      alt="Carregar Mais..."
+      title={props.movie.title} />
+  </div>
+)
+
+
 
 
 // Quando o usuário clicar em um filme (movieCover) ele verá os detalhes do filme organizados nesse compoente, que ainda não está sendo usado.
-class MovieInfo extends Component {
-  render(){
-    return(
-      <div className="movie-info">
-        <div className="movie-info-header"> 
-          {this.props.movie.title}
-        </div>
-        <div className="movie-info-body"> 
-          <p> 
-          {this.props.movie.synopsis}
-          </p>
-        </div>
-        <div className="movie-info-footer"> 
-        </div>
-      </div>
-    )
-  }
-}
+const MovieInfo = props => (
+  <div className="movie-info">
+    <div className="movie-info-header"> 
+      {this.props.movie.title}
+    </div>
+    <div className="movie-info-body"> 
+      <p> 
+      {this.props.movie.synopsis}
+      </p>
+    </div>
+    <div className="movie-info-footer"> 
+    </div>
+  </div>
+)
+
 
 
 //Container Principal que será usado como base da lista de filmes
@@ -90,7 +85,7 @@ export default class MovieContainer extends Component {
     );
   }
 
-  
+
   getMovieList() {
     axios.get('https://yts.am/api/v2/list_movies.json')
     .then(res => {
