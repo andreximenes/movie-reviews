e811c1904a1dea8069b0ff6559a27f16d0c9c219
Andre Luiz Ximenes Goncalves da Luz <andre.luz@linx.com.br>
2018-05-30 11:33:38 -0300

    organizada para ficar melhor de Garcia entender

--- src/components/movieInfoComponent.jsx
+++ src/components/movieInfoComponent.jsx
@@ -2,6 +2,24 @@ import React, { Component } from 'react';
 import axios from 'axios'
 import './style.css'
 
+// Header Container
+const HeaderContainer = props => (
+  <div className="top-container">
+    <div className="logo"> 
+      <h1>Review</h1>
+    </div>
+    <div className="busca">
+      <input type="text" />
+      <button className="botao-busca">Procurar</button>
+    </div>
+    <div className="general-info">
+      <b>Total de Filmes: </b>
+      <span> {props.totalMovies}</span>
+    </div>  
+  </div>
+)
+
+// MovieCover usado para listar os filmes, onde o mesmo será um link para acessar os detalhes do filme.
 class MovieCover extends Component {
   render(){
     return(
@@ -15,6 +33,8 @@ class MovieCover extends Component {
   }
 }
 
+
+// Quando o usuário clicar em um filme (movieCover) ele verá os detalhes do filme organizados nesse compoente, que ainda não está sendo usado.
 class MovieInfo extends Component {
   render(){
     return(
@@ -34,6 +54,8 @@ class MovieInfo extends Component {
   }
 }
 
+
+//Container Principal que será usado como base da lista de filmes
 export default class MovieContainer extends Component {
 
   constructor(props) {
@@ -42,7 +64,6 @@ export default class MovieContainer extends Component {
       movieList: [],
       loading: true
     }
-
   }
 
   componentDidMount() {
@@ -50,6 +71,26 @@ export default class MovieContainer extends Component {
     this.setState({loading:false})
   } 
 
+  
+  render() {
+    return (
+      <div>
+        <HeaderContainer totalMovies ={ this.state.movieList.length } />
+
+        <div className="movie-container loader">
+            {
+              this.state.movieList.map(movie => {
+                return(
+                  <MovieCover key={movie.id} movie={movie} />
+                )
+              })
+            }
+        </div>
+      </div>
+    );
+  }
+
+  
   getMovieList() {
     axios.get('https://yts.am/api/v2/list_movies.json')
     .then(res => {
@@ -64,35 +105,6 @@ export default class MovieContainer extends Component {
         const movies = res.data
         this.setState({movieList: movies.data.movies})
     })  
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="top-container">
-          <div className="logo"> 
-            <h1>Review</h1>
-          </div>
-          <div className="busca">
-            <input type="text" />
-            <button className="botao-busca">Procurar</button>
-          </div>
-          <div className="general-info">
-            <b>Total de Filmes: </b>
-            <span> {this.state.movieList.length}</span>
-          </div>  
-        </div>
-        <div className="movie-container loader">
-            {
-              this.state.movieList.map(movie => {
-                return(
-                  <MovieCover key={movie.id} movie={movie} />
-                )
-              })
-            }
-        </div>
-      </div>
-    );
   }
 }
 
